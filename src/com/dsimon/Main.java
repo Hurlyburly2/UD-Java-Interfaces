2f1caa99e05dfbfef191a4003c74fcac32c582e7
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-18 02:48:31 -0400

    some comments for future reference

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,14 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
+        // INHERITANCE:
+            // Limited because you can only inherit from one thing
+
+        // INTERFACES:
+            // Class can use several interfaces
+            // For example, a mobile phone is like a computer, but still could use a phone interface
+            // But also can use a web browser or computer interface
+
         ITelephone dougsPhone;
         dougsPhone = new DeskPhone(1234567);
 
